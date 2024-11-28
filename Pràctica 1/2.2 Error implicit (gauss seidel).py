39e231fccd58910f8392322360f4ebbb22e66a3e
JordiCostaGayoso <jordicosta2004@gmail.com>
2024-11-28 10:48:52 +0100

    Optimitzat i amb plots macos (comparació +error)

--- Pràctica 1/2.2 Error implicit (gauss seidel).py
+++ Pràctica 1/2.2 Error implicit (gauss seidel).py
@@ -7,56 +7,7 @@ X = np.linspace(0, 1, n)
 dx = 1 / (n - 1)  
 t_a = 0.025  
 T_0=(0.02**2*944000)/(0.56)
-
-def gauss_seidel(A, b): #aqui nomes estem omplint una columna, faltarà iterar sobre el temps. 
-    x = np.zeros_like(b)  
-
-    for _ in range(len(b)):  
-        for i in range(len(b)):  
-            sum1 = np.dot(A[i, :i], x[:i]) 
-            sum2 = np.dot(A[i, i+1:], x[i+1:])  
-            x[i] = (b[i] - sum1 - sum2) / A[i, i]  
-    return x
-
-
-def matriu(n, gamma):
-    A = np.zeros((n-2, n-2)) #la matriu és en realitat una matriu nxm, però com tenim 2 condicions de contorm, tenim n-2 files. i serien m-1 columnes però em surt error si no es quadrada. Imagino q deu ser igual.
-    for i in range(n-2):
-        A[i, i] = 1 + 2 * gamma
-        if i > 0:
-            A[i, i-1] = -gamma
-        if i < n-3:
-            A[i, i+1] = -gamma
-    return A
-
-
-def solucio(gamma, n, t_a, dx):
-    dt = gamma * dx**2
-    m = int(t_a / dt) + 1  
-    T = np.zeros((n, m))  
-    T[:, 0] = 309.65/T_0  
-    T[0, :] = 309.65/T_0  
-    T[-1, :] = 309.65/T_0   
-    
-    A = matriu(n, gamma)
-    
-    
-    for t in range(1, m):
-        b = T[1:-1, t-1] + dt  #b és la columna de Temperatura a t-1, sense tenir en comte les files i columnes que ja hem ompler amb les condicions incials
-        b[0] += gamma * T[0, t]  
-        b[-1] += gamma * T[-1, t]  
-        
-        
-        T[1:-1, t] = gauss_seidel(A, b) #omplim la resta de la matriu amb gauss-seidel
-    
-    return T*T_0
-
-
-solucions = []
-for gamma in gammas:
-    T = solucio(gamma, n, t_a, dx)
-    solucions.append((T[:, -1], gamma))  
-
+L0=2
 
 
 # Definim la funció que calcula T_tilda
@@ -97,31 +48,70 @@ T_exacte=[]
 for i in z:
     T_values=T_tilda(t, i, T_c)*T_0
     T_exacte.append(T_values)
+
+
+
+
+for gamma in gammas:
+    filename=f"matriuT_implGS_gamma_{gamma}.txt"
+    all_T = np.loadtxt(filename)
+    T=all_T[:,-1]
+    dt = gamma * dx**2
+    m = int(t_a / dt) + 1  
+    ymin=np.min(T)-273.15
+    ymax=np.max(T)-273.15
+
+    #volem fer una evolució temporal, pel que a cada plot cal plotejar cada columna. 
+    fig, ax = plt.subplots(figsize=(5,4))
+    ax.tick_params(axis='x', which='both', top=True, labeltop=False, direction='in')  
+    ax.tick_params(axis='y', which='both', right=True, labelright=False, direction='in')
+    ymin=np.min(T)-273.15
+    ymax=np.max(T)-273.15
+    ax.plot(X*L0,np.array(T_exacte)-273.15, label='Solució analítica')
+    ax.scatter(X * L0, T - 273.15, label=f"$\\gamma = {gamma}$\n$\\hat{{t}}=0.025$")
+    ax.hlines(50,0,2, color='black', linestyles='dashed', alpha=0.7)
+
+    ax.fill_betweenx([ymin, ymax + 2], 0.75, 1.25, color='lightcoral', alpha=0.5, edgecolor='none')
+    ax.set_ylim(ymin, ymax+2)
+    ax.set_xlim(0,2)
     
-#Comparació
-#gamma 0.5
-temperatures = solucions[0][0]
-temperatures_array = np.array(temperatures)
-solucions_array = np.array(T_exacte)  
+    ax.set_xlabel("z (cm)")
+    ax.set_ylabel(r"T "+'('+r'$\circ$'+'C)')
 
-print(len(T_exacte))
-print(len(solucions[0][0]))
+    ax.legend(loc='upper right')
+    plt.show()
+    #plt.savefig('Euler_implGS_' + str(gamma) + '.png', bbox_inches='tight', dpi=300)
 
-# Operació element a element
-err_r1 = np.abs((temperatures_array - solucions_array) / solucions_array)
-print(err_r1)
+    
+#Comparació i errors
 
-#gamma 1
-temperatures = solucions[1][0]
-temperatures_array = np.array(temperatures)
-solucions_array = np.array(T_exacte)  
-#err_r = (T_exacte - temperatures) / temperatures
-print(len(T_exacte))
-print(len(solucions[1][0]))
+fig_err, ax1 = plt.subplots(figsize=(5,4))
+ax1.tick_params(axis='x', which='both', top=True, labeltop=False, direction='in')  
+ax1.tick_params(axis='y', which='both', right=True, labelright=False, direction='in')
 
-# Operació element a element
-err_r2 = np.abs((temperatures_array - solucions_array) / solucions_array)
-print(err_r2)
 
-plt.plot(z,err_r1,label="gamma=0.5")
-plt.plot(z,err_r2,label="gamma=1")
+for gamma in gammas:
+    filename=f"matriuT_implGS_gamma_{gamma}.txt"
+    all_T = np.loadtxt(filename)
+    T=all_T[:,-1]
+
+
+    err = np.abs((T - np.array(T_exacte)) / np.array(T_exacte))
+
+    ymin_err = np.min(err)
+    ymax_err = np.max(err)
+
+    ax1.plot(X * L0, err, label=f"$\\gamma = {gamma}$")
+    ax1.set_ylim(0, ymax_err+0.000001)
+
+ax1.fill_betweenx([ymin_err, ymax_err+0.000001], 0.75, 1.25, color='lightcoral', alpha=0.5, edgecolor='none')
+
+
+ax1.set_xlim(0, 2)
+ax1.set_xlabel("z (cm)")
+ax1.set_ylabel("Error relatiu")
+ax1.legend(loc="upper right")
+plt.show(fig_err)
+plt.savefig('Erros_implGS_.png', bbox_inches='tight', dpi=300)
+
+
