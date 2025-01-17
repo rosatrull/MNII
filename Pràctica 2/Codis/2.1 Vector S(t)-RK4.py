4e6f1155087f13456889a173783ff1ca88307a58
rosatrull <1669711@uab.cat>
2025-01-17 10:27:00 +0100

    Update 2.1 Vector S(t)-RK4.py

--- Pràctica 2/Codis/2.1 Vector S(t)-RK4.py
+++ Pràctica 2/Codis/2.1 Vector S(t)-RK4.py
@@ -17,9 +17,10 @@ plt.rcParams.update({
 
 
 #DADES DISTÀNCIA SOL TERRA
-d_ST=np.loadtxt(f"RK4.txt",delimiter=",",skiprows=1)
+d_ST=np.loadtxt(f"RK4.txt", delimiter="," ,skiprows=1)
 x_ST=d_ST[:,0]
 y_ST=d_ST[:,1]
+z_ST=d_ST[:,2]
 
 #CONSTANTS I PARÀMETRES FÍSICS NECESSÀRIS
 G=6.67428e-11                                     #cte grav universal [m^3·kg^-1·s^-1]
@@ -30,17 +31,18 @@ T_any_s=365.25636*24*3600                         #any sideral [s]
 omega=2*np.pi/T_dia_s                             #freqüència de rotació Terra [rad/s]
 R_T=6.371e6                                       #radi de la terra [m]
 
-alpha=np.radians(23.44)                           #inclinació eix de la Terra [rad]
+alpha=np.radians(-23.44)                           #inclinació eix de la Terra [rad]
+
 M_r_a=np.array([
-    [1, 0, 0],
-    [0, np.cos(alpha), np.sin(alpha)],
-    [0, -np.sin(alpha), np.cos(alpha)]])          #matriu rotació alpha al voltant d'x
+    [np.cos(alpha), 0, np.sin(alpha)],
+    [0, 1, 0],
+    [-np.sin(alpha), 0, np.cos(alpha)]])          #matriu rotació alpha al voltant d'y (rotem el pla xz)
 
 # línia eix de rotació per després posar-la en el plot
 linia = np.linspace(0, 6e6, 100)                   # longitud de la línea
 # Coordenadas de la línea
-lx = np.zeros_like(linia)                         # La línea está en el plano yz (x=0)
-ly = linia * np.sin(alpha)                        # Proyección en el eje y
+lx = linia * np.sin(alpha)                        # Proyección en el eje x
+ly = np.zeros_like(linia)                         # La línea está en el plano xz (y=0)
 lz = linia * np.cos(alpha)                        # Proyección en el eje z
 
 #NORMALITZACIÓ
@@ -53,54 +55,55 @@ N=int(T_any_s/T_dia_s*24)                         #fem un pas cada hora
 dt = T_any_s/(t_0*(N-1))                          #mida del pas temporal
 
 #CONDICIONS INICIALS
-latitud_c=np.radians(45)                          #latitud de Caldes de Malavella [rad] 41.8356600
-longitud_c=np.radians(2.8127200)                  #longitud de Caldes de Malavella [rad]
-
+latitud_c=np.radians(41.9831100)                  #latitud de Girona [rad] 41.9831100
+longitud_c=np.radians(2.82493)                    #longitud de Girona [rad] 2.82493
 
 #VECTOR S(t)
 t=0
 p_C_terr=np.zeros((N,3))
-pos_Caldes=np.zeros((N,3))
+pos_Girona=np.zeros((N,3))                        #vector Sol-Girona
+
+# Coordenades esfèriques inicials
+theta = np.pi / 2 - latitud_c
+phi_0 = longitud_c
+R_C_0 = np.array([
+    R_T * np.sin(theta) * np.cos(phi_0),
+    R_T * np.sin(theta) * np.sin(phi_0),
+    R_T * np.cos(theta)
+])
+
+# Bucle temporal
 for j in range(1, N):
     t = t + dt
 
-    #Vector posició Caldes respect el centre de la Terra
-    theta=np.pi/2-latitud_c
-    phi_0=longitud_c
-    phi=omega*t*t_0+phi_0
+    # Longitud actual (amb rotació de la Terra)
+    phi = omega * (t*t_0) + phi_0
+
+    # Coordenades cartesianes temporals respecte al centre de la Terra
+    x_C = R_T * np.sin(theta) * np.cos(phi)
+    y_C = R_T * np.sin(theta) * np.sin(phi)
+    z_C = R_T * np.cos(theta)
+    R_C_prima = np.array([x_C, y_C, z_C])
+
+    # Rotació per la inclinació de l'eix terrestre
+    R_C_0_r = np.dot(M_r_a, R_C_0)  # Posició inicial rotada
+    p_C_terr[0] = R_C_0_r
+
+    R_C = np.dot(M_r_a, R_C_prima)  # Evolució temporal rotada
+    p_C_terr[j] = R_C
     
-    #Posició inical
-    R_C_0=[R_T*np.sin(theta)*np.cos(phi_0), R_T*np.sin(theta)*np.sin(phi_0), R_T*np.cos(theta)]
-    #Evolució temporal
-    x_C=R_T*np.sin(theta)*np.cos(phi)
-    y_C=R_T*np.sin(theta)*np.sin(phi)
-    z_C=R_T*np.cos(theta)
-    R_C_prima=np.array((x_C,y_C,z_C))
-    
-    #Rotació eix Terra
+    #Vector posició Girona respecte el Sol
     #Posició inicial
-    R_C_0_r=np.dot(M_r_a,R_C_0)
-    p_C_terr[0]=R_C_0_r
+    pos_Girona[0][0]=x_ST[0]+p_C_terr[0][0]
+    pos_Girona[0][1]=y_ST[0]+p_C_terr[0][1]
+    pos_Girona[0][2]=p_C_terr[0][2]
     #Evolució temporal
-    R_C=np.dot(M_r_a,R_C_prima)
-    p_C_terr[j]=R_C
-    
-    #Vector posició Caldes respect Sol
-    #Posició inicial
-    pos_Caldes[0][0]=x_ST[0]+p_C_terr[0][0]
-    pos_Caldes[0][1]=y_ST[0]+p_C_terr[0][1]
-    pos_Caldes[0][2]=p_C_terr[0][2]
-    #Evolució temporal
-    pos_Caldes[j][0]=x_ST[j]+p_C_terr[j][0]
-    pos_Caldes[j][1]=y_ST[j]+p_C_terr[j][1]
-    pos_Caldes[j][2]=p_C_terr[j][2]
- 
-#GUARDEM EL VECTOR S(t) EN UN FITXER    
-S_t= np.array(pos_Caldes)
-filename = f"S(t) RK4.txt"
-np.savetxt(filename, S_t, delimiter=",", header="x,y,z", comments="")
-      
-#ORBITA CALDES AL VOLTANT DEL CENTRE DE LA TERRA---------------------------------------------------------------
+    pos_Girona[j][0]=x_ST[j]+p_C_terr[j][0]
+    pos_Girona[j][1]=y_ST[j]+p_C_terr[j][1]
+    pos_Girona[j][2]=z_ST[j]+p_C_terr[j][2]          #recordem que z_ST és zero per tot t ja que haviem col·local el sol a (0,0,0)
+
+
+#ORBITA Girona AL VOLTANT DEL CENTRE DE LA TERRA---------------------------------------------------------------
 xr=p_C_terr[:,0]
 yr=p_C_terr[:,1]
 zr=p_C_terr[:,2]
@@ -125,11 +128,11 @@ ax.yaxis.pane.set_facecolor((0.9, 0.9, 0.9, 1.0))
 ax.zaxis.pane.set_facecolor((0.9, 0.9, 0.9, 1.0))  
 
 ax.scatter(xr[0], yr[0], zr[0], color='red', label="Punt inicial")             # Posició inicial
-ax.plot(xr, yr, zr, label="Trajectòria de Caldes", color='blue')               # Trajectòries
+ax.plot(xr, yr, zr, label="Trajectòria de Girona", color='blue')               # Trajectòries
 ax.scatter(0,0,0, color="black", label="Centre Terra",s=40)                    # Centre de la Terra
 ax.plot(lx, ly, lz, label='Eix de rotació', color='black', linestyle='--')
 
-ax.set_title("Posició de Caldes respecte el centre de la Terra")
+#ax.set_title("Posició de Girona respecte el centre de la Terra")
 ax.set_xlabel("x (m)")
 ax.set_ylabel("y (m)")
 ax.set_zlabel("z (m)")
@@ -137,10 +140,10 @@ ax.legend(loc='upper right')
 plt.show()
 
 
-#ORBITA CALDES AL VOLTANT DEL SOL--------------------------------------------------------------------------------
-xr=pos_Caldes[:,0]
-yr=pos_Caldes[:,1]
-zr=pos_Caldes[:,2]
+#ORBITA Girona AL VOLTANT DEL SOL--------------------------------------------------------------------------------
+xr=pos_Girona[:,0]
+yr=pos_Girona[:,1]
+zr=pos_Girona[:,2]
 
 fig = plt.figure(figsize=(5, 5), dpi = 300)
 ax = fig.add_subplot(111, projection='3d')
@@ -165,7 +168,7 @@ ax.scatter(xr[0], yr[0], zr[0], color='red', label="Punt inicial (x, y, z)")   #
 ax.plot(xr, yr, zr, label="Trajectòria (x, y, z)", color='blue')               # Trajectòries
 ax.scatter(0,0,0, color="orange", label="Sol",s=40)                            # Sol
 
-ax.set_title("Posició de Caldes respecte el Sol")
+#ax.set_title("Posició de Girona respecte el Sol")
 ax.set_xlabel("x (m)")
 ax.set_ylabel("y (m)")
 ax.set_zlabel("z (m)")
